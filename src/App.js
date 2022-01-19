787557753f284d2224af16a2d66cbf1c38057582
surya13399 <suryaskcet@gmail.com>
2022-01-19 03:34:51 -0800

    augray

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/img/group-14295.png"
+     alt="navbar"
+    />
+
+    <div class="Be-prepared-for-the-Mountains-and-beyond">
+  A place to feel better, 
+     wherever you go!
+    </div>  
+    <div class="Equipment">
+    <span class="Equipment">
+  How may I help you?
+</span>
+    </div>
+    <div>
+      <img src="/img/Group 14980.png" alt="1" class="Group-14980"/>
+      
+      <img src="/img/Group 14981.png" alt="2" class="Group 14981"/>
+      
+      
+      <img src="/img/Group 14985.png" alt="3" class="Group 14985"/>
+      
+      
+      <img src="/img/Group 14987.png" alt="4" class="Group 14987"/>
+      
+      
+      
+    </div>
+    <div className='-\32'>
+      <img src='/img/02 2 1.jpg' alt='' />
+    </div>
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
